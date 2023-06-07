bf27cbf6cfe069f28824269678a939015101f0a3
andreasscalas <andreasscalas@tiscali.it>
2023-06-07 15:41:16 +0200

    Delete build-minSizeRel directory

--- build-minSizeRel/CMakeFiles/Test-DrawableGeometries.dir/src/Geometric_primitives/drawablecube.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Test-DrawableGeometries.dir/src/Geometric_primitives/drawablecube.cpp.o: \
- /home/andreas/Documenti/Progetti/DrawableGeometries/src/Geometric\ primitives/drawablecube.cpp \
- /usr/include/stdc-predef.h
